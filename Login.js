e63213281bf87f53f0a0e29fb8c64bb3acfa6b58
VictorEyeEagle <victor100bol@hotmail.com>
2023-11-28 02:09:25 -0300

    .

--- Login.js
+++ Login.js
@@ -1,18 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View, TouchableOpacity, TextInput, Image } from 'react-native';
+import { Text, View, TouchableOpacity, TextInput, Image, Alert } from 'react-native';
 import styles from "./styles"
+import { auth } from './firebase/firebaseConfig';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useState } from 'react';
+
 
 export default function TelaInicial({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
 
-  
+  const handleLogin = async () => {
+    const autenticacao = auth;
+    try {
+      const userCredential = await signInWithEmailAndPassword(autenticacao, email, senha);
+      const user = userCredential.user;
+      console.log("Conta logada");
+      Alert.alert(
+        'Sucesso!',
+        'Voce Logou!',
+        [
+          { text: 'OK', onPress: () => navigation.navigate("Lista") }
+        ],
+        { cancelable: false }
+      );
+    } catch (error) {
+      console.error('Erro ao registrar:', error.message);
+      Alert.alert(
+        'Credenciais Invalidas',
+        'Tente Novamente',
+        [
+          { text: 'OK' }
+        ],
+        { cancelable: false }
+      );
+    }
+  };
+
+
+
   return (
     <View style={styles.container}>
       <Text style={styles.cabeçalho}>Bem vindo!</Text>
       <StatusBar style="auto" />
-      <TextInput placeholder='Email' textContentType="emailAddress" style={styles.inputtext} />
-      <TextInput placeholder='Senha' textContentType="password" secureTextEntry style={styles.inputtext} />
-      <TouchableOpacity style={styles.logButton} onPress={() => navigation.navigate("Lista")}>
+      <TextInput placeholder='Email' textContentType="emailAddress" style={styles.inputtext} value={email} onChangeText={setEmail} />
+      <TextInput placeholder='Senha' textContentType="password" secureTextEntry style={styles.inputtext} value={senha} onChangeText={setSenha} />
+      <TouchableOpacity style={styles.logButton} onPress={handleLogin}>
         <Text style={styles.buttonText}>Fazer Login</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.regButton} onPress={() => navigation.navigate("Registrar")} >
